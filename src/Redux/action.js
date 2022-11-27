2f8f494da5240b4c9d683b48fac9a7d40f1eb6e9
Prajapati <avaneesh@intellimedianetworks.net>
2022-11-27 22:35:03 +0530

    some fixeses

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,26 +1,22 @@
-import {ADD_PRODUCT, DELETE_PRODUCT, EDIT_PRODUCT} from './constant'
+import { ADD_PRODUCT, DELETE_PRODUCT, EDIT_PRODUCT } from "./constant";
 
+export const addProduct = (data) => {
+  return {
+    type: ADD_PRODUCT,
+    productData: data,
+  };
+};
 
-export const addProduct = (data) =>{
-    // console.log(data,"%%%%%%%%%%")
-    return {
-        type:ADD_PRODUCT,
-        productData : data
-    }
-}
+export const deleteProduct = (product_id) => {
+  return {
+    type: DELETE_PRODUCT,
+    productId: product_id,
+  };
+};
 
-export const deleteProduct = (product_id) =>{
-    console.log(product_id,"in delete action")
-    return {
-        type:DELETE_PRODUCT,
-        productId:product_id
-    }
-}
-
-export const editProduct = (data) =>{
-    console.log("in edit action","%%%%%%%%%%")
-    return {
-        type:EDIT_PRODUCT,
-        editProduct : data
-    }
-}
+export const editProduct = (data) => {
+  return {
+    type: EDIT_PRODUCT,
+    editProduct: data,
+  };
+};
--- src/Redux/product/product.reducer.js
+++ src/Redux/product/product.reducer.js
@@ -3,7 +3,6 @@ import { ADD_PRODUCT } from "../constant";
 export const productItems = (data = [], action) => {
   switch (action.type) {
     case ADD_PRODUCT:{
-      // console.log(action.productData,"OOOOOOOO")
       return [...data,action.productData]
     }
     default:
